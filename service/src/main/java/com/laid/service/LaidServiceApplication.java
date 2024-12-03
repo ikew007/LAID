5547bd4a3059bc6ce71122afdf247adcfd3b9a81
Andrii Pyroh <andrewpyrih@gmail.com>
2024-12-03 20:00:27 +0200

    proj init

---
+++ service/src/main/java/com/laid/service/LaidServiceApplication.java
@@ -0,0 +1,13 @@
+package com.laid.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaidServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaidServiceApplication.class, args);
+	}
+
+}
