9715472cc4d03295de9df4041023b5b0112fdf08
Andrii Pyroh <andrewpyrih@gmail.com>
2024-12-26 17:50:09 +0200

    enhanced client profile form 🚀🎨

--- client/laid_client/src/pages/profile/index.tsx
+++ client/laid_client/src/pages/profile/index.tsx
@@ -1,7 +1,13 @@
 import Profile from "./containers/Profile.tsx";
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
 
 function Index() {
-  return <Profile />;
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <Profile/>
+    </LocalizationProvider>
+  );
 }
 
 export default Index;
