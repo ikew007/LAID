83bd73a8a52691513fffd0eb195f458f72703f04
Andrii Pyroh <andrewpyrih@gmail.com>
2024-12-27 15:56:20 +0200

    enhanced client security flow logic🚀🎨

--- client/laid_client/src/app/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/laid_client/src/app/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
-import {useAuth} from "../../../hooks/useAuth.ts";
+import { useAuth } from "../../../hooks/useAuth.ts";
 
 interface ProtectedRouteProps {
   children: JSX.Element;
